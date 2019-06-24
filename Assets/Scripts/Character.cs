6aa4a3ed155cd70e000b7cd90090cc3d2d2ba5e8
Lee Jihye <jihae0584@gmail.com>
2019-06-25 01:43:43 +0900

    Make stats

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //character's stat
+    private float _intelligence;
+
+    private float _maxStamina;
+    private float _curStamina;
+
+    private float _maxFassion;
+    private float _curFassion;
+
+    private float _maxSociability;
+    private float _curSociability;
+
+
+    public float Intelli
     {
-        
+        get { return _intelligence; }
+
+        set
+        {
+            _intelligence = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public float MaxStamina
     {
-        
+        get { return _maxStamina; }
+
+        set
+        {
+            _maxStamina = value;
+        }
+    }
+
+    public float CurStamina
+    {
+        get { return _curStamina; }
+
+        set
+        {
+            _curStamina = value;
+        }
+    }
+
+    public float MaxFassion
+    {
+        get { return _maxFassion; }
+
+        set
+        {
+            _maxFassion = value;
+        }
+    }
+
+    public float CurFassion
+    {
+        get { return _curFassion; }
+
+        set
+        {
+            _curFassion = value;
+        }
+    }
+
+    public float MaxSocial
+    {
+        get { return _maxSociability; }
+
+        set
+        {
+            _maxSociability = value;
+        }
+    }
+
+    public float CurSocial
+    {
+        get { return _curSociability; }
+
+        set
+        {
+            _curSociability = value;
+        }
     }
 }
