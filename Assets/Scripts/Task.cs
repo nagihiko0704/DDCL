d4c8b83abf58a4f41bc0ff88efc0e530c0492c0d
Lee jihye <jihae0584@gmail.com>
2019-06-26 00:17:43 +0900

    일정 종류별로 구현
    휴식은 기획자가 안나타난 관계로 남겨둠

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Task : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //stats that task make player's change
+    public float staminaVal;
+    public float socialVal;
 
-    // Update is called once per frame
-    void Update()
+    //where is task in scedule
+    public (int, int) sceduleLocation; 
+
+
+    //need to add event
+}
+
+
+public class Study : Task
+{
+    public Study((int, int) _sceduleLocation)
     {
-        
+        this.staminaVal = -5f;
+        this.socialVal = 0;
+
+        this.sceduleLocation = _sceduleLocation;
+    }       
+}
+public class Club : Task
+{
+    public Club((int, int) _sceduleLocation)
+    {
+        this.staminaVal = -8f;
+        this.socialVal = 0;
+
+        this.sceduleLocation = _sceduleLocation;
+    }
+}
+
+public class Rest : Task
+{
+    public Rest((int, int) _sceduleLocation)
+    {
+        //I don't know
     }
 }
