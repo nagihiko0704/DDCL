023da1e55bff8bbb2bae2fd4cc9fcb6c995e796c
Lee jihye <jihae0584@gmail.com>
2019-06-28 01:24:26 +0900

    스케쥴 UI 구현
    모든 일정 수행 뒤에 다음날 일정 뜨게 함
    학업, 동아리, 휴식 별로 배경 다르게 함
    일정칸에 알맞은 일정 텍스트 띄움

--- Assets/Scripts/Managers/MainGameUIManager.cs
+++ Assets/Scripts/Managers/MainGameUIManager.cs
@@ -1,18 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainGameUIManager : MonoBehaviour
 {
+    //schedule window
+    public GameObject[] scheduleList = new GameObject[6];
+
+    //task background
+    
+
+    //this game's schedule
+    private Task _curTask;
+    private Schedule _curSchedule;
+    private int _curPeriod;
+    private int _curDay;
+     
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _curTask = GameManager.Inst.currentTask;
+        _curSchedule = GameManager.Inst.currentSchedule;
+        _curPeriod = _curTask.scheduleLocation.Item1;
+        _curDay = _curTask.scheduleLocation.Item2;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_curDay != GameManager.Inst.currentTask.scheduleLocation.Item2)
+        {
+            _curDay = GameManager.Inst.currentTask.scheduleLocation.Item2;
+        }
+
+        SetScheduleUI();
     }
+
+    private void SetScheduleUI()
+    {
+        Task tempTask;
+        Color colorTaskBg;
+        string taskName;
+
+        //for every task in current schedule
+        for (int task = 0; task < 6; task++)
+        {
+            tempTask = _curSchedule.taskArray[task, _curDay];
+            colorTaskBg = new Color(0f, 0f, 0f);
+            taskName = "삐\n빅";
+
+            //chage background as task type
+            //color change is for test
+            if(tempTask.GetType() == typeof(Study))
+            {
+                colorTaskBg = new Color(0.25f, 0.25f, 0);
+                taskName = "학\n\n업";
+            }
+            else if(tempTask.GetType() == typeof(Club))
+            {
+                colorTaskBg = new Color(0.25f, 0, 0.25f);
+                taskName = "동\n아\n리";
+            }
+            else if(tempTask.GetType() == typeof(Rest))
+            {
+                colorTaskBg = new Color(0, 0.25f, 0.25f);
+                taskName = "휴\n\n식";
+            }
+
+            //applicate background and text
+            scheduleList[task].GetComponent<Image>().color = colorTaskBg;
+            scheduleList[task].GetComponentInChildren<Text>().text = taskName;
+        }
+    }
+
+   
 }
