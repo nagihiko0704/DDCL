8a73ec8da59d1d37a3ead1e1eef5759c8c38deb2
Lee jihye <jihae0584@gmail.com>
2019-06-27 22:33:18 +0900

    AddTask 메서드 구현
    Schedule 클래스의 메서드임
    Schedule에 Task를 추가하는 메서드

--- Assets/Scripts/Schedule.cs
+++ Assets/Scripts/Schedule.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Schedule
 {
-    public Task[,] taskArray = new Task[6, 7];
+    //Schedule has tasks and nth week num
+    public Task[,] taskArray = new Task[6, 5];
+    public int scheduleWeek;
 
     public Schedule()
     {
+        scheduleWeek = 1;
+        
+        //intiate with rest task
         for(int row = 0; row < 6; row++)
         {
             for(int col = 0; col < 7; col++)
@@ -15,5 +20,36 @@ public class Schedule
                 this.taskArray[row, col] = new Rest((row, col));
             }
         }
+    }
+
+    public void AddTask(Player player, Task taskType)
+    {
+        int period = taskType.scheduleLocation.Item1;
+        int day = taskType.scheduleLocation.Item2;
+
+        for(int week = 1; week <= 16; week++)
+        {
+            taskType.scheduleLocation = (period, day);
+            player.schedules[week - 1].taskArray[period, day] = taskType;
+            player.schedules[week - 1].scheduleWeek = week;
+        }
+    }
+
+    public void AddTask(Schedule schedule, Task taskType)
+    {
+        int period = taskType.scheduleLocation.Item1;
+        int day = taskType.scheduleLocation.Item2;
+
+        taskType.scheduleLocation = (period, day);
+        schedule.taskArray[period, day] = taskType;
+    }
+
+    public void AddTask(Task taskType)
+    {
+        int period = taskType.scheduleLocation.Item1;
+        int day = taskType.scheduleLocation.Item2;
+
+        taskType.scheduleLocation = (period, day);
+        this.taskArray[period, day] = taskType;
     }
 }
