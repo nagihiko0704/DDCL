ded5f7fc0ab0ea0c292a33ddb3cb7ae13d135e15
JeongWooSeok <www013181@gmail.com>
2019-06-29 22:16:14 +0900

    이벤트 클래스에 결과 메세지 변수 추가

--- Assets/Scripts/Event.cs
+++ Assets/Scripts/Event.cs
@@ -14,67 +14,12 @@ public class Event : ScriptableObject
 
     public Sprite situation;
 
+    //TODO: add minigame
+
     public List<string> choice;
+
+    public List<string> result;
 
     public float staminaVal;
     public float socialVal;
-
-    
-
-
-    /*public Event()
-    {
-        this._eventCode = 0;
-        this._eventProbability = 0;
-        this._staminaVal = 0;
-        this._socialVal = 0;
-    }
-
-    public Event(float stamina, float social)
-    {
-        this._eventCode = 0;
-        this._eventProbability = 0;
-        this._staminaVal = stamina;
-        this._socialVal = social;
-    }
-    
-    public int EventCode
-    {
-        get { return _eventCode; }
-
-        set
-        {
-            _eventCode = value;
-        }
-    }
-
-    public float EventProbability
-    {
-        get { return _eventProbability; }
-
-        set
-        {
-            _eventProbability = value;
-        }
-    }
-
-    public float Stamina
-    {
-        get { return _staminaVal; }
-
-        set
-        {
-            _staminaVal = value;
-        }
-    }
-
-    public float Social
-    {
-        get { return _socialVal; }
-
-        set
-        {
-            _socialVal = value;
-        }
-    }*/
 }
