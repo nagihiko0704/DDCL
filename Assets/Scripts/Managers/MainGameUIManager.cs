518e51a5fd6879e5ce8151842b7832550f4afa77
Jaehyung Doh <knuppe22@gmail.com>
2019-06-30 02:44:37 +0900

    MainGameUIManager 싱글톤으로 변경

--- Assets/Scripts/Managers/MainGameUIManager.cs
+++ Assets/Scripts/Managers/MainGameUIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainGameUIManager : MonoBehaviour
+public class MainGameUIManager : SingletonBehaviour<MainGameUIManager>
 {
     //schedule window
     public GameObject[] scheduleList = new GameObject[6];
