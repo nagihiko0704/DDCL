80484ea2593dd30e88369e6bd0b38054b37d1a88
chenzhige <tsar3701@korea.ac.kr>
2019-07-09 16:05:43 +0800

    강의결과 씬과 매니저를 생성함, 유아이 대충 만듬

---
+++ Assets/Scripts/Lecture.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Lecture", menuName ="Lecture")]
+public class Lecture : ScriptableObject
+{
+    public string lectureName;
+    public string lectureClassification;
+    public string lectureRating;
+}
