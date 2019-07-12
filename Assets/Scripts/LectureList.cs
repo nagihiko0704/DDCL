3a967a1f8db9056ca6dcb30bf62898abda4f1b1a
chenzhige <tsar3701@korea.ac.kr>
2019-07-12 17:26:40 +0800

    LectureResultManager구현 WIP

--- Assets/Scripts/LectureList.cs
+++ Assets/Scripts/LectureList.cs
@@ -10,26 +10,26 @@ public class LectureList
     {
         //add lecture list: name, type, grade
 
-        this.lectureList.Add("S", new Study("암벽등반", Type.Sport, "S"));
-        this.lectureList.Add("S", new Study("논리설계", Type.Major, "S"));
-        this.lectureList.Add("S", new Study("현대 시의 탐구와 이해", Type.Discuss, "S"));
+        this.lectureList.Add("S01", new Study("암벽등반", Type.Sport, "S"));
+        this.lectureList.Add("S02", new Study("논리설계", Type.Major, "S"));
+        this.lectureList.Add("S03", new Study("현대 시의 탐구와 이해", Type.Discuss, "S"));
 
-        this.lectureList.Add("A", new Study("컴퓨터 구조", Type.Major, "A"));
-        this.lectureList.Add("A", new Study("회로 이론", Type.Major, "A"));
-        this.lectureList.Add("A", new Study("자유는 정의와 진리를 싣고", Type.Discuss, "A"));
-        this.lectureList.Add("A", new Study("소프트볼", Type.Sport, "A"));
-        this.lectureList.Add("A", new Study("농구", Type.Sport, "A"));
+        this.lectureList.Add("A01", new Study("컴퓨터 구조", Type.Major, "A"));
+        this.lectureList.Add("A02", new Study("회로 이론", Type.Major, "A"));
+        this.lectureList.Add("A03", new Study("자유는 정의와 진리를 싣고", Type.Discuss, "A"));
+        this.lectureList.Add("A04", new Study("소프트볼", Type.Sport, "A"));
+        this.lectureList.Add("A05", new Study("농구", Type.Sport, "A"));
 
-        this.lectureList.Add("B", new Study("컴퓨터 프로그래밍", Type.Major, "B"));
-        this.lectureList.Add("B", new Study("선형대수학", Type.Major, "B"));
-        this.lectureList.Add("B", new Study("컴퓨터 네트워크", Type.Major, "B"));
-        this.lectureList.Add("B", new Study("현대 철학 입문", Type.Discuss, "B"));
-        this.lectureList.Add("B", new Study("연극 관람과 이해", Type.Discuss, "B"));
-        this.lectureList.Add("B", new Study("축구", Type.Sport, "B"));
+        this.lectureList.Add("B01", new Study("컴퓨터 프로그래밍", Type.Major, "B"));
+        this.lectureList.Add("B02", new Study("선형대수학", Type.Major, "B"));
+        this.lectureList.Add("B03", new Study("컴퓨터 네트워크", Type.Major, "B"));
+        this.lectureList.Add("B04", new Study("현대 철학 입문", Type.Discuss, "B"));
+        this.lectureList.Add("B05", new Study("연극 관람과 이해", Type.Discuss, "B"));
+        this.lectureList.Add("B06", new Study("축구", Type.Sport, "B"));
 
-        this.lectureList.Add("C", new Study("데이터 통신", Type.Major, "C"));
-        this.lectureList.Add("C", new Study("법학통론", Type.Discuss, "C"));
-        this.lectureList.Add("C", new Study("스포츠 댄스", Type.Sport, "C"));
-        this.lectureList.Add("C", new Study("피겨 댄스", Type.Sport, "C"));
+        this.lectureList.Add("C01", new Study("데이터 통신", Type.Major, "C"));
+        this.lectureList.Add("C02", new Study("법학통론", Type.Discuss, "C"));
+        this.lectureList.Add("C03", new Study("스포츠 댄스", Type.Sport, "C"));
+        this.lectureList.Add("C04", new Study("피겨 댄스", Type.Sport, "C"));
     }
 }
--- Assets/Scripts/Managers/LectureResultGameManager.cs
+++ Assets/Scripts/Managers/LectureResultGameManager.cs
@@ -1,23 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LectureResultGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] gameLecture = new GameObject[5];
+
+    public LectureList list = new LectureList();
+
+    private int[] lectureFinalScore = new int[5];
+    private string[] lectureFinalGrade = new string[5];
+    public string[] finalLecture = new string[5];
+
+    private 
 
     void Start()
     {
-
+        SetLecture();
     }
 
-    public int[] lectureFinalScore = new int[5];
+    
     void SetLecture()
     {
         for (int i = 0; i < 4; i++)
         {
             lectureFinalScore[i] = GameManager.Inst.lectureApplicationScore[i] + GameManager.Inst.lectureChoiceScore[i];
+
+            if (lectureFinalScore[i] >= 20)
+            {
+                //s
+                foreach(KeyValuePair<string,Task> item in list.lectureList)
+                {
+                    string found = "S";
+                    var lectureS=list.lectureList.Where()
+
+
+                }
+            }
+            else if (lectureFinalScore[i] >= 17)
+                //a
+            else if (lectureFinalScore[i] >= 11)
+                //b
+            else
+                //c
+
         }
     }
 
+    public void Lecture0()
+    {
+
+    }
+
+    public void Lecture1()
+    {
+
+    }
+
+    public void Lecture2()
+    {
+
+    }
+
+    public void Lecture3()
+    {
+
+    }
+
+    public void Lecture4()
+    {
+
+    }
 }
