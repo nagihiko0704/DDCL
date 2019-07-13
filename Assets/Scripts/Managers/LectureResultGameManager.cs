902d27d8c32f4db5949ca46ffddb8849495b4739
chenzhige <tsar3701@korea.ac.kr>
2019-07-13 12:42:58 +0800

    수강결과 코드 완성

--- Assets/Scripts/Managers/LectureResultGameManager.cs
+++ Assets/Scripts/Managers/LectureResultGameManager.cs
@@ -8,19 +8,16 @@ public class LectureResultGameManager : MonoBehaviour
     public GameObject[] gameLecture = new GameObject[5];
 
     public LectureList list = new LectureList();
+    public List<Task> tempList = new List<Task>();
 
     private int[] lectureFinalScore = new int[5];
-    private string[] lectureFinalGrade = new string[5];
-    public string[] finalLecture = new string[5];
 
-    private 
 
     void Start()
     {
         SetLecture();
     }
 
-    
     void SetLecture()
     {
         for (int i = 0; i < 4; i++)
@@ -30,40 +27,48 @@ public class LectureResultGameManager : MonoBehaviour
             if (lectureFinalScore[i] >= 20)
             {
                 //s
-                
+                var countS = list.lectureList["S"].Count;
+                int lectureNumS = Random.Range(0, countS);
+                tempList = list.lectureList["S"];
+
+
+                gameLecture[i].GetComponent<Text>().text = tempList[lectureNumS].taskName +"    S급";
             }
             else if (lectureFinalScore[i] >= 17)
+            {
                 //a
+                var countA = list.lectureList["A"].Count;
+                int lectureNumA = Random.Range(0, countA);
+                tempList = list.lectureList["A"];
+
+
+                gameLecture[i].GetComponent<Text>().text = tempList[lectureNumA].taskName + "    A급";
+            }
+                
             else if (lectureFinalScore[i] >= 11)
+            {
                 //b
+                var countB = list.lectureList["B"].Count;
+                int lectureNumB = Random.Range(0, countB);
+                tempList = list.lectureList["B"];
+
+
+                gameLecture[i].GetComponent<Text>().text = tempList[lectureNumB].taskName + "    B급";
+            }
+
             else
+            {
                 //c
+                var countC = list.lectureList["C"].Count;
+                int lectureNumC = Random.Range(0, countC);
+                tempList = list.lectureList["C"];
+
+
+                gameLecture[i].GetComponent<Text>().text = tempList[lectureNumC].taskName + "    C급";
+            }
 
         }
     }
 
-    public void Lecture0()
-    {
-
-    }
-
-    public void Lecture1()
-    {
-
-    }
-
-    public void Lecture2()
-    {
-
-    }
-
-    public void Lecture3()
-    {
-
-    }
-
-    public void Lecture4()
-    {
-
-    }
+   
 }
