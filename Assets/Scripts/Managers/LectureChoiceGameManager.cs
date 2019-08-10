57a2e59fb4503ac2c7b851723a4ba8a852652d07
chenzhige <tsar3701@korea.ac.kr>
2019-08-11 01:11:35 +0800

    화나고 행복한 사람 랜덤생성wip
    점수가 다 1이 뜸 ㅎ

--- Assets/Scripts/Managers/LectureChoiceGameManager.cs
+++ Assets/Scripts/Managers/LectureChoiceGameManager.cs
@@ -7,10 +7,16 @@ using UnityEngine.SceneManagement;
 
 public class LectureChoiceGameManager : MonoBehaviour
 {
-    
+
     public GameObject[] buttonLectures = new GameObject[4];
     public Sprite[] manResoure = new Sprite[3];//0 is non, 1 is happy, 2 is angry
     public GameObject[] happyAndAngry = new GameObject[13];
+
+    public GameObject[] haa0 = new GameObject[13];
+    public GameObject[] haa1 = new GameObject[13];
+    public GameObject[] haa2 = new GameObject[13];
+    public GameObject[] haa3 = new GameObject[13];
+
 
     //array element value is decending order of goodness
     //value range: 0-3
@@ -24,7 +30,7 @@ public class LectureChoiceGameManager : MonoBehaviour
     private int choiceNumNow = 0;
 
     private const float TIME_LIMIT = 4;
-    private float limitTime=TIME_LIMIT;
+    private float limitTime = TIME_LIMIT;
 
     private readonly int[] SCORE = { 10, 7, 4, 1 };
 
@@ -40,92 +46,84 @@ public class LectureChoiceGameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		//timer
-		float time = Time.deltaTime;
-		limitTime -= time;
+        //timer
+        float time = Time.deltaTime;
+        limitTime -= time;
 
         //To make sure you made your choice in time.
-		if (limitTime <= 0)
-		{
-			if (_choiceNum == choiceNumNow)
-			{
-				GameManager.lectureChoiceScore[_choiceNum] = 1;
-				limitTime = TIME_LIMIT;
-				Debug.Log(1);
-				_choiceNum++;
-			}
-		}
+        if (limitTime <= 0)
+        {
+            if (_choiceNum == choiceNumNow)
+            {
+                GameManager.lectureChoiceScore[_choiceNum] = 1;
+                limitTime = TIME_LIMIT;
+                Debug.Log(1);
+                _choiceNum++;
+            }
+        }
 
         //if u play the game 5 times, change to application scene.
-		if (_choiceNum == 5)
+        if (_choiceNum == 5)
         {
             SceneManager.LoadScene(2);
             //Destroy(this.gameObject);
         }
 
-		//if u already made ur choice, do LectureChoiceGame() again.
-		if (_choiceNum == choiceNumNow + 1)
+        //if u already made ur choice, do LectureChoiceGame() again.
+        if (_choiceNum == choiceNumNow + 1)
         {
             LectureChoiceGame();
             choiceNumNow++;
         }
-        
+
     }
 
     //set lecture order by goodness
     //apply ui
     private void LectureChoiceGame()
     {
-        for(int i = 0; i < 4; i++)
-        {
-            _goodLectureOrder[i] = i;
-            _happyEach[i] = Random.Range(0, 13);
-        }
-
-        for(int i = 0; i < _happyEach.Length - 1; i++)
-        {
-            for(int j = i + 1; j < _happyEach.Length; j++)
-            {
-                int temp;
-                if (_happyEach[i] > _happyEach[j])
-                {
-                    temp = _happyEach[i];
-                    _happyEach[i] = _happyEach[j];
-                    _happyEach[j] = temp;
-                }
-            }
-        }//sort _happyEach
-        
-
-        Shuffle(_goodLectureOrder);
         for (int i = 0; i < 4; i++)
         {
-            int x=_goodLectureOrder[i];
-            setLectureChoiceMan(_happyEach[x],x);
+            _goodLectureOrder[i] = i;
+        }
+
+        SetHappy(_happyEach);
+
+        Shuffle(_goodLectureOrder, _happyEach);
+
+        for (int i = 0; i < 4; i++)
+        {
+            int x = _goodLectureOrder[i];
+            SetLectureChoiceMan(_happyEach[i],i);
         }
     }
 
     //shuffles elements in array
-    private void Shuffle(int[] array)
+    private void Shuffle(int[] array1, int[] array2)
     {
-        int n = array.Count();
+        int n = 4;
 
         while (n > 1)
         {
             n--;
             int i = Random.Range(0, n + 1);
-            int temp = array[i];
-            array[i] = array[n];
-            array[n] = temp;
+
+            int temp = array1[i];
+            array1[i] = array1[n];
+            array1[n] = temp;
+
+            temp = array2[i];
+            array1[i] = array1[n];
+            array1[n] = temp;
         }
     }
 
     public void OnLecture0ButtonClicked()
     {
-       
+
         int goodOrder = _goodLectureOrder[0];
         GameManager.lectureChoiceScore[_choiceNum] = SCORE[goodOrder];
-        
+
         Debug.Log(SCORE[goodOrder]);
         _choiceNum++;
     }
@@ -135,7 +133,7 @@ public class LectureChoiceGameManager : MonoBehaviour
     {
         int order1 = _goodLectureOrder[1];
         GameManager.lectureChoiceScore[_choiceNum] = SCORE[order1];
-		
+
         Debug.Log(SCORE[order1]);
         _choiceNum++;
     }
@@ -145,7 +143,7 @@ public class LectureChoiceGameManager : MonoBehaviour
     {
         int order2 = _goodLectureOrder[2];
         GameManager.lectureChoiceScore[_choiceNum] = SCORE[order2];
-		
+
         Debug.Log(SCORE[order2]);
         _choiceNum++;
     }
@@ -155,56 +153,55 @@ public class LectureChoiceGameManager : MonoBehaviour
     {
         int order3 = _goodLectureOrder[3];
         GameManager.lectureChoiceScore[_choiceNum] = SCORE[order3];
-		
+
         Debug.Log(SCORE[order3]);
         _choiceNum++;
     }
 
-    void setLectureChoiceMan(int happyMan,int goodLecture) {
+    void SetLectureChoiceMan(int happyMan, int goodLecture)
+    {
         int totalMan;
-        int angryMan;
+        //int angryMan;
         int noMan;
         int[] tempList = new int[13];
-        
+
 
         //set the amount of people
-        if (happyMan >= 8)
+        if (happyMan > 8)
             totalMan = Random.Range(happyMan, 13);
         else
             totalMan = Random.Range(8, 13);
 
         //set the amout of angry people and non-people place
         noMan = 13 - totalMan;
-        angryMan = totalMan - happyMan;
 
         //make a array[13], value is 0, 1, or 2.
         //non -people=>0, happy=>1, angry=>2
-        for(int i = 0; i < noMan; i++){
-            tempList[i] = 0;
-        }
-        for(int i=noMan; i < noMan + happyMan; i++)
+        int count = 0;
+        for (;count<noMan;count++)
         {
-            tempList[i] = 1;
+            tempList[count] = 0;
         }
-        for(int i = noMan + happyMan; i < noMan + happyMan + angryMan; i++)
+        for (; count < noMan + happyMan; count++)
         {
-            tempList[i] = 2;
+            tempList[count] = 1;
+        }
+        for (; count < 13; count++)
+        {
+            tempList[count] = 2;
         }
 
         //mix the array
-        for(int i=0; i < 13; i++)
+        for (int i = 0; i < 13; i++)
         {
-            int temp=tempList[i];
-            int randomOne = Random.Range(0, 13);
-
-            tempList[i] = tempList[randomOne];
-            tempList[randomOne] = temp;
+            Swap(i, Random.Range(0,13), tempList);
         }
 
         //set the sprite
-        for(int i = 0; i < 13; i++)
+        for (int i = 0; i < 13; i++)
         {
-            switch(tempList[i]){
+            switch (tempList[i])
+            {
                 case 0:
                     happyAndAngry[i].GetComponent<Image>().sprite = manResoure[0];
                     break;
@@ -217,5 +214,61 @@ public class LectureChoiceGameManager : MonoBehaviour
             }
         }
 
+        switch (goodLecture)
+        {
+            case 0:
+                for (int i = 0; i < 13; i++)
+                    haa0[i].GetComponent<Image>().sprite = happyAndAngry[i].GetComponent<Image>().sprite;
+                break;
+            case 1:
+                for (int i = 0; i < 13; i++)
+                    haa1[i].GetComponent<Image>().sprite = happyAndAngry[i].GetComponent<Image>().sprite;
+                break;
+            case 2:
+                for (int i = 0; i < 13; i++)
+                    haa2[i].GetComponent<Image>().sprite = happyAndAngry[i].GetComponent<Image>().sprite;
+                break;
+            case 3:
+                for (int i = 0; i < 13; i++)
+                    haa3[i].GetComponent<Image>().sprite = happyAndAngry[i].GetComponent<Image>().sprite;
+                break;
+        }
+
+    }
+
+    void Swap(int one, int two, int[] arr)
+    {
+        int temp = arr[one];
+        arr[one] = arr[two];
+        arr[two] = temp;
+    }
+
+    void SetHappy(int[] arr)
+    {
+        //make happy
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int num = Random.Range(0, 13);
+            while (arr.Contains(num))
+            {
+                num = Random.Range(0, 13);
+            }
+            arr[i] = num;
+        }
+
+        //sort happy
+        for(int i = 0; i < arr.Length - 1; i++)
+        {
+            for(int j=i+1; j < arr.Length; j++)
+            {
+                if (i < j)
+                {
+                    int temp = i;
+                    i = j;
+                    j = temp;
+                }
+            }
+        }
     }
 }
+    
