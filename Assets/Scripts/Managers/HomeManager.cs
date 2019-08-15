22fd476dbe2609c67ceff7a93c2747e43ecc7d57
chenzhige <tsar3701@korea.ac.kr>
2019-08-15 17:56:27 +0800

    홈 메니저 스켈레톤

---
+++ Assets/Scripts/Managers/HomeManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class HomeManager : MonoBehaviour
+{
+    /****
+     * 0: setting
+     * 1: new character
+     * 2: existing character
+     * 3: achievement
+     ****/
+    public GameObject[] button = new GameObject[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnclickBuccon0()
+    {
+        //??????
+    }
+
+    void OnclickButton1()
+    {
+        //scence load "new character" scene.
+        //SceneManager.LoadScene();
+    }
+
+    void OnClickButton2()
+    {
+        //scence load main scene???
+    }
+
+    void OnClickButton3()
+    {
+        //scence load "achievement" scene.
+        //SceneManager.LoadSecne();
+    }
+}
