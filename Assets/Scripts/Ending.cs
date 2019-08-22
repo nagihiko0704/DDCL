4e0436fe00c1b4a4891f1063e109de9dcf39d913
chenzhige <tsar3701@korea.ac.kr>
2019-08-23 02:31:52 +0800

    엔딩에 성적뜨게함 wip(슈레딩거임)

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    Score sscore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            sscore.SetGradeCredit(GameManager.Inst.studyResultArray[i], i, GameManager.Inst.studyResultArray[i].Favor);
+            Debug.Log("과목" + i + ":     " + GameManager.Inst.studyResultArray[i].Score);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
