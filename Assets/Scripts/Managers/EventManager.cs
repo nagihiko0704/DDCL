da84058d907d5493f8b48ad34bf8ae9bf7b76396
Jaehyung Doh <knuppe22@gmail.com>
2019-08-23 01:48:50 +0900

    미니게임을 프리팹 형식으로 수정

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class EventManager : SingletonBehaviour<EventManager>
 {
-
     public GameObject eventPopUpWindow;
 
     //miniGameList[0] => Bulb Catch
@@ -15,24 +14,12 @@ public class EventManager : SingletonBehaviour<EventManager>
     //miniGameList[3] => 뒤집힌 카드 맞추기
     //miniGameList[4] => 파이프 게임
     public List<GameObject> miniGameList = new List<GameObject>();
+    public Transform miniGamePanel;
 
     public int miniGameResult;
     public int eventResultIndex;
 
     public int choiceNum;
-
-    //bulb catch
-    public GameObject bulb;
-    public Button buttonBulbCatch;
-
-    public float bulbSpeed = 1000f;
-    public float direction = 1f;
-
-    public float[] bulbMiniGameScore = new float[3];
-
-    public int bulbGameNum = 0;
-
-    public bool isBulbGamePlaying = false;
 
     // Start is called before the first frame update
     void Start()
@@ -50,21 +37,6 @@ public class EventManager : SingletonBehaviour<EventManager>
     // Update is called once per frame
     void Update()
     {
-        if (bulb.transform.localPosition.x > 350f
-            || bulb.transform.localPosition.x < -350f)
-        {
-            direction *= -1;
-        }
-
-        if (isBulbGamePlaying)
-        {
-            bulb.transform.Translate(Vector3.right * direction * bulbSpeed * Time.deltaTime);
-        }
-        else
-        {
-            bulbGameNum = 0;
-        }
-
         if (!(ScheduleManager.Inst.doEvent))
         {
             eventResultIndex = -1;
@@ -265,80 +237,17 @@ public class EventManager : SingletonBehaviour<EventManager>
     }
 
     //Event method
-
     private void MiniGameBulbCatch()
     {
-        miniGameList[0].SetActive(true);
-
-        direction = 1f;
-
-        bulb.transform.localPosition = new Vector2(-350f, 424f);
-
-        isBulbGamePlaying = true;
-
-        if (bulbGameNum > 0)
-        {
-            return;
-        }
-
-        buttonBulbCatch.onClick.AddListener(() => OnBulbCatchGameButtonClick());
+        MiniGame(0);
     }
-
-    private void OnBulbCatchGameButtonClick()
+    private void MiniGameCorrectButton()
     {
-        bulbGameNum++;
-
-        Debug.Log("bulbGameNum: " + bulbGameNum);
-
-        if (bulbGameNum <= 3)
-        {
-            if (bulb.transform.localPosition.x >= -60 && bulb.transform.localPosition.x <= 60)
-            {
-                bulbMiniGameScore[bulbGameNum - 1] = 4f;
-            }
-            else if (bulb.transform.localPosition.x >= -160 && bulb.transform.localPosition.x <= 160)
-            {
-                bulbMiniGameScore[bulbGameNum - 1] = 2f;
-            }
-            else
-            {
-                bulbMiniGameScore[bulbGameNum - 1] = 0f;
-            }
-
-            if (bulbGameNum == 3)
-            {
-                isBulbGamePlaying = false;
-                bulbGameNum = 0;
-
-                float result = 0;
-
-                for (int i = 0; i < 3; i++)
-                {
-                    result += bulbMiniGameScore[i];
-                }
-
-                if (result >= 10)
-                {
-                    miniGameResult = 0;
-                }
-                else if (result >= 8)
-                {
-                    miniGameResult = 1;
-                }
-                else
-                {
-                    miniGameResult = 2;
-                }
-
-                eventPopUpWindow.GetComponent<EventPopUp>().InitResult();
-
-            }
-        }
-
-        if (bulbGameNum <= 2)
-        {
-            MiniGameBulbCatch();
-        }
+        MiniGame(1);
+    }
+    private void MiniGame(int index)
+    {
+        Instantiate(miniGameList[index], miniGamePanel);
     }
 
     private void CheckEvent1010Result()
