400dd87c83a138b0b8724dc71c73a9525772fdf9
chenzhige <tsar3701@korea.ac.kr>
2019-08-22 19:44:42 +0800

    세팅메니저 rename

---
+++ Assets/Scripts/Managers/SettingSceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingSceneManager : MonoBehaviour
+{
+    //0: setting 1: creator
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickHomeButton()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void OnClickCreatorButton()
+    {
+        canvas.SetActive(true);
+    }
+
+    public void OnClickBackButton()
+    {
+        //????!!!!
+    }
+
+    public void OnClickCreatorBackButton()
+    {
+        canvas.SetActive(false);
+    }
+}
